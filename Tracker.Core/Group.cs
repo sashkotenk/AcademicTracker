7996cb8106a8c3f31ea45f9c933f1be927256860
Oleksandr <50671144+sashkotenk@users.noreply.github.com>
2026-01-14 14:02:35 +0200

    Add files via upload

---
+++ Tracker.Core/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tracker.Core;
+
+public class Group
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty; // Наприклад "IPZ-31"
+
+    // Зв'язок: Одна група має багато студентів
+    public List<Student> Students { get; set; } = new();
+}
